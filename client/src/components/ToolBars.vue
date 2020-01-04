52fa5937a943b12080172fcd076364da1493f97e
thakorn09 <tanakon.rs@gmail.com>
2020-01-04 12:40:09 +0700

    close#34 -make ui ระบบจองห้องในโรงพยาบาลของแพทย์

---
+++ client/src/components/ToolBars.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-toolbar class="light-blue lighten-3">
+      <v-toolbar-title>ระบบข้อมูลแพทย์</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn text>เพิ่มข้อมูลเเพทย์</v-btn>
+        <v-btn text>สั่งจ่ายยา</v-btn>
+        <v-btn text>นัดผู้ป่วย</v-btn>
+        <v-btn text>จัดตารางเวร</v-btn>
+        <v-btn text>จองห้องภายในโรงพยาบาลสำหรับแพทย์</v-btn>
+        <v-btn text>ออกจากระบบ</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </div>
+</template>
