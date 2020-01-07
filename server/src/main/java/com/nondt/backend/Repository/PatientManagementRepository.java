dd08686d0403b2e90c0a57ee6b1ef8f9a12999d2
thakorn09 <tanakon.rs@gmail.com>
2020-01-08 00:19:52 +0700

    close #70 -แก้ไข Repository และ Entity BookRoom

--- server/src/main/java/com/nondt/backend/Repository/PatientManagementRepository.java
+++ server/src/main/java/com/nondt/backend/Repository/PatientManagementRepository.java
@@ -7,5 +7,4 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 public interface PatientManagementRepository extends JpaRepository<PatientManagement, Long> {
     
-
 }
